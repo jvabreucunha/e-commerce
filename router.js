e43916e4edf046b69d7748a1ad20534ff72b7cca
JOAO VICTOR DE ABREU CUNHA <joao_va_cunha@estudante.sesisenai.org.br>
2025-09-29 11:54:13 -0300

    router pronto

--- router.js
+++ router.js
@@ -12,13 +12,13 @@ router.post('/login', authController.login)
 
 router.post('/usuario', validarUsuario, usuarioController.cadastrar)
 router.get('/usuario', authMiddleware(["funcionario", "admin"]), usuarioController.listarTodos)
-router.get('/usuario:id', authMiddleware(["funcionario", "admin"]), usuarioController.buscarPorId)
-router.put('/usuario:id', authMiddleware(["cliente", "funcionario", "admin"]), validarUsuario, usuarioController.atualizar)
-router.delete('/usuario:id', authMiddleware(["cliente", "admin"]), usuarioController.deletar)
+router.get('/usuario/:id', authMiddleware(["funcionario", "admin"]), usuarioController.buscarPorId)
+router.put('/usuario/:id', authMiddleware(["cliente", "funcionario", "admin"]), validarUsuario, usuarioController.atualizar)
+router.delete('/usuario/:id', authMiddleware(["cliente", "admin"]), usuarioController.deletar)
 
 router.post('/veiculos', authMiddleware(["cliente", "funcionario", "admin"]), validarVeiculo, veiculoController.cadastrar);
 router.get('/veiculos', authMiddleware(["cliente", "funcionario", "admin"]), veiculoController.listarTodos);
-router.get('/veiculos:id', authMiddleware(["cliente", "funcionario", "admin"]), veiculoController.buscarPorId);
+router.get('/veiculos/:id', authMiddleware(["cliente", "funcionario", "admin"]), veiculoController.buscarPorId);
 router.put('/veiculos/:id', authMiddleware(["cliente", "admin"]), validarVeiculo, veiculoController.atualizar);
 router.get('/veiculos/buscar', authMiddleware(["cliente", "funcionario", "admin"]), validarModeloQuery, veiculoController.buscarPorModelo);
 
